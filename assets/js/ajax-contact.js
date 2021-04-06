3bc81aac2dedb3d835f27394f12609c8e37c67c4
Kafle Sagar <nephopsagar@gmail.com>
2021-04-06 18:56:02 +0200

    Error msg modified

--- assets/js/ajax-contact.js
+++ assets/js/ajax-contact.js
@@ -40,7 +40,7 @@ $(function() {
 			if (data.responseText !== '') {
 				$(formMessages).text(data.responseText);
 			} else {
-				$(formMessages).text('Oops! An error occured and your message could not be sent.');
+				$(formMessages).text('Oops! An error occured and your message could not be sent. Please email directly to suraj.sharma@fireballcompany.com. We are resolving the problem soon.');
 			}
 		});
 	});
